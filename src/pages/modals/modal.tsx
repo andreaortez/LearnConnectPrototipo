46b6b8f3acf47bc8b85974893b30359352e31c3c
Andrea <andreaortez11@gmail.com>
2025-02-05 13:26:03 -0600

    base de la pagina

---
+++ src/pages/modals/modal.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+interface ModalParams {
+    title: string;
+    message: string;
+    onClose: () => void;
+    footer?: React.ReactNode;
+}
+
+export default function Modal({ title, message, onClose, footer }: ModalParams) {
+    return (
+        <div className="modal show d-block" tabIndex={-1}>
+            <div className="modal-dialog modal-dialog-centered">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">{`${title}`}</h5>
+                        <button type="button" className="btn-close" aria-label="Close" onClick={onClose}></button>
+                    </div>
+                    <div className="modal-body">
+                        <p>{`${message}`}</p>
+                    </div>
+                    {footer && (
+                        <div className="modal-footer">
+                            {footer}
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
