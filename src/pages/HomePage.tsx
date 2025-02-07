bf95a368761f6f0c747500fc31f178fd1c81123b
elizabeth hernandez <eehs052811@gmail.com>
2025-02-07 13:15:52 -0600

    responsive sidebar en homepage listo

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import { useEffect } from "react";
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import Modal from './modals/modal'
+import SideBar from "./modals/SideBar";
+
+export default function HomePage (){
+    const router = useRouter();
+    useEffect(() => {
+        const initBootstrap = async () => {
+          await import('bootstrap');
+        };
+        initBootstrap();
+      }, []);
+      
+    return (
+        <div className="container-fluid">
+
+        <div className="column">
+            <aside className="col-md-3 col-lg-3 sticky-top">
+                <SideBar />
+            </aside>   
+            <main className="col-md-6 ms-sm-auto col-lg-9 px-md-4">
+                <nav id="home-header" className="navbar navbar-light bg-light d-md-none">
+                    <div className="container-fluid">
+                        <button className="navbar-toggler"
+                            type="button"
+                            data-bs-toggle="offcanvas"
+                            data-bs-target="#sidebarMenu"
+                            aria-controls="sidebarMenu"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                            >
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                    <span className="navbar-brand mb-0 h1">Homepage</span>
+                    </div>
+                </nav>   
+                    <section id="landing" className="pt-3">
+                        <h2>Bienvenido a LearnConnect</h2>
+                    </section>
+                    <section id="Actividades" className="pt-3">
+                        <h2>Actividades</h2>
+                        <p>Sube tus archivos o anotaciones para estudiar con nuestros juegos, examenes y flashcards
+                            generadas con IA!
+                        </p>
+                    </section>
+                    
+                
+            </main>
+        
+        </div>
+    </div>
+    );
+
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import "@/styles/globals.css";
 import "@/styles/styles.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Script from 'next/script';
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        strategy="afterInteractive"
+      />
   return <Component {...pageProps} />;
 }
